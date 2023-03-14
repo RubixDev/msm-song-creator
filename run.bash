6c97d874b1a128dec3dc548e8d1cf0a28dfa804f
RubixDev <silas.groh@t-online.de>
2023-03-14 17:10:36 +0100

    don't skip islands 11 and 20 in `run.bash`

--- run.bash
+++ run.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for i in {1..21}; do
-    if [ "$i" -eq 11 ] || [ "$i" -eq 20 ]; then continue; fi
-
+for i in {1..22}; do
     echo -e "\x1b[36mIsland \x1b[1m$i\x1b[22m...\x1b[0m"
     target/release/msm "$i" -o songs
     echo -e "\x1b[32m..done\x1b[0m"
